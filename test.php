d2306dd405b3b0824991bf546f60b64741b4739b
Deadx34 <isurushehan393@gmail.com>
2025-10-23 12:18:48 +0530

    Refactor AYONION-CMS structure by removing diagnostic.php, mime_test.html, test.html, and test.php files to streamline the project and eliminate unused code. Updated .htaccess to allow access to the uploads directory while preventing PHP execution in specific subdirectories for enhanced security. Improved index.php to serve as the main application interface with a modern layout and user-friendly features.

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Simple PHP test file
-echo "PHP is working!<br>";
-echo "Current time: " . date('Y-m-d H:i:s') . "<br>";
-echo "PHP Version: " . phpversion() . "<br>";
-echo "Server: " . $_SERVER['SERVER_SOFTWARE'] . "<br>";
-?>
